4fa0120de5ccabdd9bc8232566a53f4ec62f3be3
Elvisthegreat <>
2023-11-18 20:55:57 +0000

    Add multiplicattion game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,9 @@ function runGame(gameType) {
 
     if (gameType === "addition"){
         displayAdditionQuestion(num1, num2);
-    }else{
+    }else if(gameType === "multiply"){
+        displayMultiplyQuestion(num1, num2);
+    } else{
         alert(`Uknown game type: ${gameType}`);
         throw `Uknown game type: ${gameType}. Aborting!`;
     }
@@ -69,7 +71,9 @@ function calculateCorrectAnswer() {
 
     if (operator === "+"){
         return [operand1 + operand2, "addition"];
-    }else{
+    }else if(operator === "x"){
+        return [operand1 * operand2, "multiply"];
+    } else{
         alert(`Uniplemented operator ${operator}`);
         throw `Uniplemented operator ${operator}. Aborting!`;
     }
@@ -101,10 +105,18 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "-";
+
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
+
 
 }
