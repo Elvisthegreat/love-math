46933a2afa7172d933dfeb8f361fd8045ce4d692
Elvisthegreat <>
2023-11-18 16:38:43 +0000

    Add an if statement to check the plus operator

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,21 @@ function checkAnswer() {
 
 }
 
+/**
+ * Get the operands (the numbers) and the operators (plus, minus etc)
+ * directly from the DOM, and return correct answer.
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+"){
+        return [operand1 + operand2, "addition"];
+    }else{
+        alert(`Uniplemented operator ${operator}`);
+        throw `Uniplemented operator ${operator}. Aborting!`;
+    }
 
 }
 
